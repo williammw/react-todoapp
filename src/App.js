bd819fd43ee9ba7ef52ce96efe9f5887662e7ec9
wwwwwmw <william.manwai@gmail.com>
2020-09-23 01:38:50 +0800

    use ? very good trick

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
-
+function App() {  
+  const [user, setUser] = useState(null);
   // handle async with useEffect
   useEffect(() => {
     // run when app.js componenet loads, run once only    
-    fetch("https://randomuser.me/api/").then(async(result) => {
-      console.log(await result.json());
+    fetch("https://randomuser.me/api/").then(async(response) => {
+      //console.log(await result.json());
+      const user = await response.json();
+      setUser(user.results[0]);
     })
         
-  }, [])
-
+  }, [])  
   return (
     <div>
-      <h1>Hi</h1>
+      <h1>Welcome</h1>
+      <h2>Raondom person generated is</h2>
+      <h3>Name is : {user?.name?.first} {user?.name?.last}</h3>
     </div>
   );
 }
