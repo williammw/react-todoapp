6a1db81c854a20545c3b03ac71ad4de12ba63b6c
wwwwwmw <william.manwai@gmail.com>
2020-09-23 01:22:38 +0800

    useEffect and async function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  // handle async with useEffect
+  useEffect(() => {
+    // run when app.js componenet loads, run once only
+    const fetchData = async() => {
+      await fetch("https://randomuser.me/api/").then(async (result) => {
+        console.log(await result.json());
+      })
+    }
+    fetchData();
+  }, [])
+
   return (
     <div>
       <h1>Hi</h1>
