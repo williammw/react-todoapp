8cfce6034350da5a23520a476f6ff845fd933332
wwwwwmw <william.manwai@gmail.com>
2020-09-24 04:47:41 +0800

    username, login added

--- src/App.js
+++ src/App.js
@@ -5,41 +5,31 @@ import db from './firebase';
 
 
 function App() {  
-
-  const [input, setInput] = useState("")
-  const [todos, setTodos] = useState([])
-  // run useEffect when component loaded useEffect(function, array)
-  useEffect(() => { 
-    db.collection('todos').onSnapshot(snapshot => {
-      setTodos(snapshot.docs.map(doc => doc.data().title))
-    })
-  }, [])
-
-  
-  const addTodo = (e) => {
-    e.preventDefault()
-    //console.log(input)
-    // add whatever in input into array
-    // clear the input field
-    db.collection('todos').add({
-      title: input
-    })
-    // setTodos([...todos, input]);
-    setInput('')
-
-    console.log([...todos, input])
-  }
+const [username,setUsername] = useState("")
+const [password,setPassword] = useState("")
+const login = () => {
+  console.log('login dedail')
+  console.log(username)
+  console.log(password)
+}
   return (
-    <div className="App">
-      <h1>Todo</h1>
+    <div className="App">         
+    <input 
+      value={username}
+      onChange={(e) => setUsername(e.target.value)}
+      type="text"
+      placeholder="insert your username" 
+    />
+    <input 
+      value={password}
+      onChange={(e) => setPassword(e.target.value)}
+      type="password"
+      placeholder="insert your password" 
+    />
 
-      <form>
-        <input type="text" value={input} onChange={e => setInput(e.target.value)}></input>            
-        <button onClick={addTodo} type="submit" >add to do</button>
-      </form>
-      {todos.map((todo, i) =>  <Todo key={i} title={todo} /> )}
-      
-      
+    <button onClick={login}>Login</button>
+    <button>Register</button>
+
     </div>
   )
 }
